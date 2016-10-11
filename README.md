5a834d45b737fafd301bdaf871f8714930f6005b
clingsii <ling.caol@gmail.com>
2016-10-11 19:50:33 +1300

    groovy support bugfixed

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 a web application can run code of different languages
 
+- java
 ![](https://raw.githubusercontent.com/clingsii/images/master/codeRunner1.png)
+
+- groovy 
+![](https://raw.githubusercontent.com/clingsii/images/master/codeRunner2.png)
 
 - v0.1 Oct 6, 2016. Support java.
--- src/main/java/com/cl/cloud/engine/GroovyEngine.java
+++ src/main/java/com/cl/cloud/engine/GroovyEngine.java
@@ -1,8 +1,15 @@
 package com.cl.cloud.engine;
 
 import com.cl.cloud.domain.ExecutionResult;
+import com.google.common.collect.Lists;
+import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import groovy.lang.Script;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 /**
  * Created by Ling Cao on 2016/10/11.
@@ -10,13 +17,18 @@ import groovy.lang.Script;
 public class GroovyEngine implements CodeEngine {
     @Override
     public ExecutionResult execute(String code) {
-        GroovyShell shell = new GroovyShell();
-        Script script = shell.parse(code);
-        Object result = script.run();
+
+        ByteArrayOutputStream bytesOutput = new ByteArrayOutputStream();
+
+        PrintStream output = new PrintStream(bytesOutput);
+        Binding binding = new Binding();
+        binding.setProperty("out", new PrintStream(output));
+        GroovyShell shell = new GroovyShell(binding);
+        shell.run(code, "test", Lists.newArrayList());
 
         ExecutionResult executionResult = new ExecutionResult();
         executionResult.setSuccess(true);
-        executionResult.setOutput(result.toString());
+        executionResult.setOutput(bytesOutput.toString());
 
         return executionResult;
     }
